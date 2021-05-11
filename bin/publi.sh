af8b906344e54444c108990f64ed1d73e39c6357
Glenn Y. Rolland <glenux@glenux.net>
2021-05-12 01:19:52 +0200

    chore: add publi.sh script

---
+++ bin/publi.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# change the branch names appropriately
+set -x
+set -u
+set -e
+
+BRANCH=public-pages
+
+# Cleanup
+git worktree remove -f _site || true
+rm -rf _site/ || true
+
+# Link to branch
+git worktree add -B $BRANCH _site origin/$BRANCH || true
+
+# Build
+hugo
+
+# Commit new changes
+cd public
+git add --all
+git commit -m "`date`"
+git push origin $BRANCH
+cd ..
